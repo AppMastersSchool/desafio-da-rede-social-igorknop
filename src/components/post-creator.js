7b74593237988c59a73f13315e8605a4bbdc1093
Igor Knop <igorknop@gmail.com>
2018-12-14 12:38:16 -0200

    Fix textarea children lint

--- src/components/post-creator.js
+++ src/components/post-creator.js
@@ -1,41 +1,41 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 
 class PostCreator extends Component {
-  constructor() {
-    super();
-    this.state = {
-      text: ""
-    };
-  }
+    constructor(){
+        super();
+        this.state = {
+            text: ''
+        }
+    }
+    
+    createPost(){
+        const newPost = {
+            content: this.state.text,
+            author: 'Baraky',
+            time: new Date().getTime(),
+            initialLikes: 0
+        }
+        this.props.onCreate(newPost);
+    }
 
-  createPost() {
-    const newPost = {
-      content: this.state.text,
-      author: "Baraky",
-      time: new Date().getTime(),
-      initialLikes: 0
-    };
-    this.props.onCreate(newPost);
-  }
-
-  render() {
-    return (
-      <div style={{ padding: 15 }}>
-        <h3>Novo Post</h3>
-        <textarea
-            rows="5"
-          onChange={event => {
-            const value = event.target.value;
-            this.setState({ text: value });
-          }}
-          style={{ width: "100%" }}
-        >
-          {this.state.text}
-        </textarea>
-        <button onClick={() => this.createPost()}>Postar</button>
-      </div>
-    );
-  }
+    render(){
+        return(
+            <div style={{padding:15}}>
+                <h3>Novo Post</h3>
+                <textarea
+                onChange={(event)=>{
+                    const value = event.target.value;
+                    this.setState({text: value})
+                }}
+                value={this.state.text}
+                rows="5"
+                style={{width: '100%'}} />
+                <button onClick={()=>this.createPost()}>
+                    Postar
+                </button>
+            </div>
+        )
+    }
 }
 
 export default PostCreator;
