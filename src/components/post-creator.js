37936077bbd439250744b7f8725a2f5cbe3e170f
Igor Knop <igorknop@gmail.com>
2018-12-14 12:36:53 -0200

    Change post to text block

--- src/components/post-creator.js
+++ src/components/post-creator.js
@@ -1,40 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class PostCreator extends Component {
-    constructor(){
-        super();
-        this.state = {
-            text: ''
-        }
-    }
-    
-    createPost(){
-        const newPost = {
-            content: this.state.text,
-            author: 'Baraky',
-            time: new Date().getTime(),
-            initialLikes: 0
-        }
-        this.props.onCreate(newPost);
-    }
+  constructor() {
+    super();
+    this.state = {
+      text: ""
+    };
+  }
 
-    render(){
-        return(
-            <div style={{padding:15}}>
-                <h3>Novo Post</h3>
-                <input
-                onChange={(event)=>{
-                    const value = event.target.value;
-                    this.setState({text: value})
-                }}
-                value={this.state.text}
-                style={{width: '100%'}} />
-                <button onClick={()=>this.createPost()}>
-                    Postar
-                </button>
-            </div>
-        )
-    }
+  createPost() {
+    const newPost = {
+      content: this.state.text,
+      author: "Baraky",
+      time: new Date().getTime(),
+      initialLikes: 0
+    };
+    this.props.onCreate(newPost);
+  }
+
+  render() {
+    return (
+      <div style={{ padding: 15 }}>
+        <h3>Novo Post</h3>
+        <textarea
+            rows="5"
+          onChange={event => {
+            const value = event.target.value;
+            this.setState({ text: value });
+          }}
+          style={{ width: "100%" }}
+        >
+          {this.state.text}
+        </textarea>
+        <button onClick={() => this.createPost()}>Postar</button>
+      </div>
+    );
+  }
 }
 
 export default PostCreator;
--- src/components/post.js
+++ src/components/post.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "../post.css";
+import "./post.css";
 
 class Post extends Component {
   constructor(props) {
